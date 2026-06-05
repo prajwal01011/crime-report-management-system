784870132859c4bc9c3021ab2b912b21b3bbb3d4
Prajwal Baskota <prajwalbaskota11@gmail.com>
2026-06-05 14:38:08 +0545

    first commit

---
+++ crime-management-system.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("----------------------------------------------------\n");
+    printf("1) Create \t \t 2) Edit\n");
+    printf("3) Search \t  \t 4) Delete\n");
+    printf("----------------------------------------------------\n");
+
+return 0;
+}
