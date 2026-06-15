d7b08dffcef435a282667f22ea0fcb886a81bc66
Prajwal Baskota <prajwalbaskota11@gmail.com>
2026-06-15 20:09:57 +0545

    some improvements

--- crime-management-system.c
+++ crime-management-system.c
@@ -4,6 +4,7 @@
 =========================================================================================*/
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 #include<stdlib.h>
 //creating a structure to store criminal details
 struct criminal_record{
@@ -16,10 +17,34 @@ struct criminal_record{
 };
 //creating a function and declaring it
 int main (void); // Main function: Program execution starts here
+struct criminal_record criminal;
 void add(void);
-void add() // Function definition: Adds a new criminal record to the system
+void add(void) // Function definition: Adds a new criminal record to the system
 { 
-
+    FILE *fp;
+    fp = fopen("criminal.dat","ab");//"ab" is "a"for binary or append mode
+    //for checking if file opened successfully
+    if (fp == NULL)
+    {
+    printf("Error opening file.\n");
+    return;
+    }
+    else{
+    printf("Enter Criminal ID:");
+    scanf("%d",&criminal.criminal_id);
+    getchar();// consume the leftover newline from the previous scanf
+    printf("Enter Criminal Name:");
+    fgets(criminal.name, sizeof(criminal.name), stdin);
+    criminal.name[strcspn(criminal.name, "\n")] = '\0';//remove the unnecessary newline character created by fgets when there is enough space in the buffer
+    printf("Enter Criminal National ID number:");
+    scanf("%d",&criminal.national_id);
+    printf("Enter Criminal age:");
+    scanf("%d",&criminal.age);
+    printf("Enter Criminal Height:");
+    scanf("%f",&criminal.height);
+    fwrite(&criminal , sizeof(criminal), 1, fp );// Saves one criminal record (structure) to "criminal.dat" file in binary mode for permanent storage
+    fclose(fp);
+        }
 }
 void display(void);
 void display() // Function definition: Displays all stored criminal records
@@ -46,9 +71,9 @@ void delete() // Function definition: Removes a criminal record from the system
 }
 
 int main(){
-    top:
-    printf("\t \t  Crime Record Management System \t \t \n");
     int task;
+    while(1){
+    printf("\t \t  Crime Record Management System \t \t \n");
     printf("-----------------------------------------------------------------------------\n");
     printf("[1] Add Criminal Records \t \t [2] Display Criminal Records \n");
     printf("[3] Search  Criminal Record \t  \t [4] Modify Criminal Record \n");
@@ -81,10 +106,10 @@ int main(){
         case 6:{
             printf("you have exited the program\n");
             printf("Exited successfully\n");
-            printf("_______________________________________________\n");
+            printf("-----------------------------------------------------------------------------\n");
             printf("Thank you\n");
-            printf("_______________________________________________\n");
-            break;
+            printf("-----------------------------------------------------------------------------\n");
+            return 0;
             
         }
         default:{
@@ -92,9 +117,9 @@ int main(){
             printf("-----------------------------------------------------------------------------\n");
             printf("please enter a number defined in the function\n");
             printf("-----------------------------------------------------------------------------\n");
-             goto top;
         }
     }
     printf("-----------------------------------------------------------------------------\n");
+}
 return 0;
 }
